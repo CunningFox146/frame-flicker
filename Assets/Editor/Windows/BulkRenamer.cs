61575795dde83cb9b3757e2617df4066723465dd
Makar Dzezhamesau <dezhemesov.work@gmail.com>
2024-05-29 11:41:03 +0200

    Add bulk renamer

---
+++ Assets/Editor/Windows/BulkRenamer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Object = UnityEngine.Object;
+
+namespace GameEditor.Windows
+{
+    public class BulkRenamer : EditorWindow
+    {
+        private static Object[] _selectedObjects;
+        private TextField _inputField;
+
+        [MenuItem("Assets/Bulk Rename #f2", false, 100)]
+        public static bool BulkRename()
+        {
+            if (Selection.count <= 1)
+                return false;
+            
+            _selectedObjects = Selection.objects;
+            GetWindow<BulkRenamer>(true, "Enter file name:");
+            FocusWindowIfItsOpen<BulkRenamer>();
+            
+            return true;
+        }
+
+        private void OnFocus()
+        {
+            maxSize = new Vector2(300, 25);
+            maxSize = new Vector2(300, 25);
+            _inputField.Focus();
+        }
+
+        private void OnDestroy()
+        {
+            if (_selectedObjects is null)
+                return;
+            
+            var newName = _inputField.value;
+            if (string.IsNullOrWhiteSpace(newName))
+                return;
+
+            for (var i = 0; i < _selectedObjects.Length; i++)
+            {
+                var selectedObject = _selectedObjects[i];
+                AssetDatabase.RenameAsset(AssetDatabase.GetAssetPath(selectedObject), $"{newName}{i}");
+            }
+            AssetDatabase.SaveAssets();
+        }
+
+        private void CreateGUI()
+        {
+            var root = new VisualElement();
+            root.style.flexGrow = 1;
+            root.style.flexDirection = FlexDirection.Row;
+            
+            _inputField = new TextField();
+            _inputField.style.flexGrow = 1;
+
+            var button = new Button(Close);
+            button.text = "Rename";
+            
+            root.Add(_inputField);
+            root.Add(button);
+            rootVisualElement.Add(root);
+            
+            rootVisualElement.RegisterCallback<KeyDownEvent>(OnKeyDown, TrickleDown.TrickleDown);
+        }
+
+        private void OnKeyDown(KeyDownEvent evt)
+        {
+            if (evt.keyCode == KeyCode.Return)
+                Close();
+        }
+    }
+}
---
+++ Assets/Editor/Windows/UvSpriteGenerator.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.U2D;
+using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
+
+namespace GameEditor.Windows
+{
+    public class UvSpriteGenerator : EditorWindowBase
+    {
+        [MenuItem("Tools/Uv Sprite Generator")]
+        public static void ShowWindow() 
+            => GetWindow(typeof(UvSpriteGenerator), false, "Uv Sprite Generator", true);
+
+        private Color GetRandomColor()
+        {
+            var colors = new[]
+            {
+                Color.red,
+                Color.blue,
+                Color.green,
+                Color.yellow,
+                Color.cyan,
+                Color.magenta,
+                Color.white,
+                new Color(0.5f, 1f, 1f),
+            };
+            return colors[Random.Range(0, colors.Length)];
+        }
+
+        protected override void RenderWindow()
+        {
+            var loadSpritesButton = new Button
+            {
+                text = "Load all sprites",
+            };
+            
+            loadSpritesButton.clicked += () =>
+            {
+                foreach (var spritePath in AssetDatabase.FindAssets("t:spriteatlas", new []{ "Assets" }).Select(AssetDatabase.GUIDToAssetPath))
+                {
+                    var container = new VisualElement();
+                    container.style.flexDirection = FlexDirection.Row;
+                    container.style.flexGrow = 1;
+                    container.style.width = new Length(100f, LengthUnit.Percent);
+                    container.style.height = new Length(100f, LengthUnit.Percent);
+                    
+                    var atlas = AssetDatabase.LoadAssetAtPath<SpriteAtlas>(spritePath);
+                    var sprites = new Sprite[atlas.spriteCount];
+                    atlas.GetSprites(sprites);
+                    
+                    // var importer = (TextureImporter)AssetImporter.GetAtPath(spritePath);
+                    // AssetDatabase.ImportAsset(spritePath, ImportAssetOptions.ForceUpdate);
+                    // EditorUtility.SetDirty(importer);
+                    // importer.SaveAndReimport();
+
+                    var atlasTexture = sprites.First().texture;
+                    var texture = new Texture2D(atlasTexture.width, atlasTexture.height);
+                    
+                    foreach (var sprite in sprites)
+                    {
+                        // Debug.Log($"{sprite.name} {sprite.rect.position}");
+                        var color = GetRandomColor();
+                        var rect = sprite.rect;
+                        
+                        for (var x = 0; x < rect.width; x++)
+                        for (var y = 0; y < rect.width; y++)
+                        {
+                            texture.SetPixel((int)rect.position.x + x,(int)rect.position.y + y, color);
+                        }
+
+                    }
+                    texture.Apply();
+
+                    var main = new Image();
+                    main.style.flexGrow = 1;
+                    main.image = atlasTexture;
+
+                    var generated = new Image();
+                    generated.style.flexGrow = 1;
+                    generated.image = texture;
+                    
+                    container.Add(main);
+                    container.Add(generated);
+                    
+                    // var sinCosTexture = new Image();
+                    // sinCosTexture.image = CreateTexture(atlas.texture.width, atlas.texture.height);
+                    // container.Add(sinCosTexture);
+                    
+                    rootVisualElement.Add(container);
+                }
+                
+            };
+
+            rootVisualElement.Add(loadSpritesButton);
+        }
+        
+        private Texture2D CreateTexture(int width = 1024, int height = 1024)
+        {
+            var tex = new Texture2D(width, height);
+
+            for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++)
+            {
+                var red = x / (float)width;
+                var green = y / (float)height;
+                var color = new Color(red, green, 0f);
+                tex.SetPixel(x, y, color);
+            }
+            tex.Apply();
+            return tex;
+        }
+    }
+}
